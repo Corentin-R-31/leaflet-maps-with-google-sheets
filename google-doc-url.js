532b0f00855483c99a76fb85ed298c36899cc6ec
Corentin-R-31 <45269565+Corentin-R-31@users.noreply.github.com>
2022-11-30 19:31:57 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1gAXNAZh6a_dCV4YqwZbVWdbKEKHY4LHBq3Ke2p2f3OY/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyDOYVlbHrchBisy4dTmUPI39PciGZ6CSHw';
